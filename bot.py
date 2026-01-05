b49a73ec2051ea48188854f0aca8587184ce5933
Agang8689 <148992314+Agang8689@users.noreply.github.com>
2026-01-06 03:09:55 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -144,3 +144,14 @@ if __name__ == "__main__":
     main()
 
 
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, user_message))
+    app.add_handler(CallbackQueryHandler(button_handler))
+    app.add_handler(MessageHandler(filters.REPLY, admin_reply))
+
+    print("Bot is running...")
+    app.run_polling()
